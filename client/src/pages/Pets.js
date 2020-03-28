6ad6faa9f7aaa88ea1bcb9c37cd208a7d36bd0a6
pzubar <pmzubar@gmail.com>
2020-03-28 19:51:48 +0200

    Adds mutation to create a pet

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,30 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import gql from 'graphql-tag'
 import { useQuery, useMutation } from '@apollo/react-hooks'
 import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const CREATE_PET = gql`
+    mutation CreateAPet($newPet: NewPetInput!) {
+        addPet(input: $newPet) {
+            id
+            name
+            type
+        }
+    }
+`
+
 const ALL_PETS = gql`
     query AllPets {
-		pets {
-			id
-			name
-			type
-			img
-		}
-	}
+        pets {
+            id
+            name
+            type
+            img
+        }
+    }
 `
 export default function Pets() {
   const [modal, setModal] = useState(false)
-  const { data, loading, error } = useQuery(ALL_PETS)
+  const { data: petsData, loading: petsLoading, error } = useQuery(ALL_PETS)
+  const [createPet, newPet] = useMutation(CREATE_PET)
 
   const onSubmit = input => {
 	setModal(false)
+	createPet({ variables: { newPet: input } })
   }
 
-  if (loading) {
+  if (petsLoading || newPet.loading) {
 	return <Loader/>
+  }
+
+  if (error || newPet.error) {
+	return <p>Error occurred: {JSON.stringify(error || newPet.error)}</p>
   }
 
   if (modal) {
@@ -45,7 +61,7 @@ export default function Pets() {
 		  </div>
 		</section>
 		<section>
-		  <PetsList pets={data.pets}/>
+		  <PetsList pets={petsData.pets}/>
 		</section>
 	  </div>
   )
